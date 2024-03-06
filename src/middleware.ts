b8835ec21a7ebd87865ffce78d69d4eed139344c
Udaky <rizkyfakhri227@gmail.com>
2024-03-06 20:41:21 +0700

    changing path

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 import { analytics } from "./utils/analytics";
 
 export default async function middleware(req: NextRequest) {
-  if (req.nextUrl.pathname === "/analytics") {
+  if (req.nextUrl.pathname === "/") {
     // track analytics event
     try {
       analytics.track("pageview", {
-        page: "/analytics",
+        page: "/",
         country: req.geo?.country,
       });
     } catch (err) {
